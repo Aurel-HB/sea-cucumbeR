85a92b64ae53ed1eacd3805d50fc992bd8483afe
Aurel HEBERT-BURGGRAEVE <ahebertb@IFREMER.FR>
2025-12-17 11:54:10 +0100

    Bottom temp process maj

--- environment_exploration/Bottom_temp_process.R
+++ environment_exploration/Bottom_temp_process.R
@@ -11,58 +11,126 @@ library(RColorBrewer)
 library(sf)
 library(viridis)
 library(ggplot2)
+library(raster)
+library(stars)
+library(dplyr)
+
+#load the study area
+calcul_area <- readRDS(paste(here(),
+                             "/SIG/SIG_Data/study_calcul_area.rds",sep=""))
 
 #open a netCDF file 
-ncin <- nc_open(paste(here(),
-                      "/environment_exploration/Environment_Data/row/",
-                      "cmems_mod_glo_phy_my_0.083deg_P1M-m_1765382048849.nc",
-                      sep=""))
-print(ncin)
+ncrast <- raster::stack(paste(here(),
+                        "/environment_exploration/Environment_Data/row/",
+                        "cmems_mod_glo_phy_my_0.083deg_P1M-m_1765967013203.nc",
+                        sep=""), varname = "bottomT")
+  
+print(ncrast)
 
-# extract dimension of the file ####
-# get longitude and latitude
-lon <- ncvar_get(ncin,"longitude")
-nlon <- dim(lon)
-head(lon)
+# Get a data.frame with raster cell values ####
+tmp_df <- as.data.frame(ncrast, xy = TRUE) 
 
-lat <- ncvar_get(ncin,"latitude")
-nlat <- dim(lat)
-head(lat)
-
-# get time
-time <- ncvar_get(ncin,"time")
-time
-
-tunits <- ncatt_get(ncin,"time","units")
-tunits
-nt <- dim(time)
-
-# get depth
-depth <- ncvar_get(ncin,"depth")
-ndepth <- dim(depth)
-head(ndepth)
+name <- c()
+for (i in 1:dim(ncrast)[3]){
+  months <- c("Jan","Feb","Mar","Apr","May",
+              "Jun","Jul","Aug","Sep","Oct","Nov",
+              "Dec")
+  month <- months[time_cf[i,2]]
+  name <- c(name,paste(month, time_cf[i,1],sep = "_"))
+}
+names(tmp_df) <- c("lon","lat",name)
 
 
-# get variable sea floor temperature
-variable <- "bottomT"
-tmp_array <- ncvar_get(ncin,variable)
-dlname <- ncatt_get(ncin,variable,"long_name")
-dunits <- ncatt_get(ncin,variable,"units")
-fillvalue <- ncatt_get(ncin,variable,"_FillValue")
-dim(tmp_array)
+# transform dataframe in sf
+sea_floor_tmp <- st_as_sf(tmp_df02, crs = "EPSG:4326", coords = c("lon","lat"))
 
-# Reshaping from raster to rectangular ####
 
-#Convert the time variable
-#convert time to CFtime class
-cf <- CFtime(tunits$value, calendar = "gregorian", time)
+#map the tmp ####
+ggplot(sea_floor_tmp['May_2025'])+
+  geom_sf(aes(color=May_2025))+
+  scale_color_viridis()+
+  theme(aspect.ratio = 1,
+        legend.title = element_blank(),
+        title = element_text(color = "black",face = "bold"),
+        plot.title = element_text( size = 12, hjust = 0.5),
+        plot.subtitle = element_text(size = 8,hjust = 0.5),
+        panel.border = element_blank(),
+        panel.grid.major = element_line(linewidth = 0.25, linetype = 'solid',
+                                        colour = "white"),
+        panel.background = element_rect(fill = "#d0d1e6"),
+        panel.grid.minor = element_line(linewidth = 0.25, linetype = 'solid',
+                                        colour = "white"))+
+  coord_sf(xlim = c(-56.5,-56), ylim = c(45,46), expand = FALSE)+
+  xlab("")+ylab("")+
+  labs(title = "Banc de Saint Pierre et Zone d'étude du projet")
 
-timestamps <- as_timestamp(cf) # get character-string times
-timestamps
-class(timestamps)
+#fuse the raster with the grid of prediction used ####
 
-#parse the string into date components
-time_cf <- parse_timestamps(cf, timestamps)
-time_cf
-class(time_cf)
+# use the study area polygon to generate an extrapolation grid
+coordinate <- as.data.frame(calcul_area[[1]][[1]][[1]])/1000
+names(coordinate) <- c("X","Y")
+res <- 0.5
+grid <- expand.grid(
+  long = seq(min(coordinate$X), max(coordinate$X), by = res),
+  lat = seq(min(coordinate$Y), max(coordinate$Y), by = res)
+)
+plot(grid)
+
+grid <- grid %>%
+  mutate(X = long*1000)%>%
+  mutate(Y= lat*1000)%>%
+  st_as_sf(crs = "EPSG:4467",
+           coords = c("X","Y"))
+
+# convert the coordinate of the raster to the epsg 4467 of SPM
+ncrast_SPM <- projectRaster(ncrast, crs = "epsg:4467", res = 500)
+tmp_df_SPM <- as.data.frame(ncrast_SPM, xy = TRUE) 
+names(tmp_df_SPM) <- c("lon","lat",name)
+
+plot(ncrast_SPM)
+
+# transform dataframe in sf
+sea_floor_tmp_SPM <- st_as_sf(tmp_df_SPM, crs = "EPSG:4467",
+                              coords = c("lon","lat"))
+
+#map the tmp #
+ggplot(sea_floor_tmp_SPM['May_2025'])+
+  geom_sf(aes(color=May_2025))+
+  scale_color_viridis()+
+  geom_sf(data=calcul_area, fill = "#11111100")+
+  geom_point(data=grid, aes(x=long*1000,y=lat*1000), color = "red")+
+  theme(aspect.ratio = 1,
+        legend.title = element_blank(),
+        title = element_text(color = "black",face = "bold"),
+        plot.title = element_text( size = 12, hjust = 0.5),
+        plot.subtitle = element_text(size = 8,hjust = 0.5),
+        panel.border = element_blank(),
+        panel.grid.major = element_line(linewidth = 0.25, linetype = 'solid',
+                                        colour = "white"),
+        panel.background = element_rect(fill = "#d0d1e6"),
+        panel.grid.minor = element_line(linewidth = 0.25, linetype = 'solid',
+                                        colour = "white"))+
+  xlab("")+ylab("")+
+  labs(title = "Banc de Saint Pierre et Zone d'étude du projet")
+
+# save the raster to open it as a stars object and convert in sf
+writeRaster(ncrast_SPM,
+            paste(here(),"/environment_exploration/Environment_Data/processed/",
+                  "BottomT_500",sep = ""),
+                             format = "GTiff")
+ncstars=read_stars(paste(
+  here(),"/environment_exploration/Environment_Data/processed/",
+                         "BottomT_500.tif",sep = ""))
+
+# transform dataframe in sf
+sea_floor_tmp_SPM <- st_as_sf(ncstars, crs = "EPSG:4467",
+                              coords = c("lon","lat"))
+names(sea_floor_tmp_SPM) <- c(name,"geometry")
+saveRDS(sea_floor_tmp_SPM,
+        paste(here(),"/environment_exploration/Environment_Data/processed/",
+                                 "BottomT_500.rds",sep = ""))
+
+# finally join the prediction grid with the sea_floor_tmp_SPM sf
+grid_bottom_tmp <- 
+  st_join(grid, sea_floor_tmp_SPM) # to get intersection of points and poly
 
