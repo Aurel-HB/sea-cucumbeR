0089777d272a93109d50ad5220969d4c4fca6866
Aurel HEBERT-BURGGRAEVE <ahebertb@IFREMER.FR>
2026-02-13 16:48:46 +0100

    maj point process

--- point_process/Script/Point_process_modelling.R
+++ point_process/Script/Point_process_modelling.R
@@ -18,8 +18,24 @@ list_PPP <- readRDS(paste(
   here(),"/via3_data_exploration/Data/processed/list_PPP_tuyau_2025.rds",
   sep=""))
 
+data_position <- readRDS(paste(
+  here(),"/via3_data_exploration/Data/processed/data_position_2025.rds",
+  sep=""))
+
+HOLOTVSPM2025 <- readRDS(paste(here(),
+              "/comparing_biomass_method/Data/03_HOLOTVSPM2025_summary.rds",
+              sep=""))
 
 ############
 # Start with one PPP
 ############
+
+PPP <- list_PPP[[1]]
+stn <- names(list_PPP)[1]
+d <- 
+
+# spatialization of the PPP
+
+
+
 
